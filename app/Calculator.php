06bde5759bf0bc1dfb713036934621e8ad779a4b
mouson <mouson@gmail.com>
2017-04-20 01:30:34 +0800

    Calculator Step 14: refactoring production code

--- app/Calculator.php
+++ app/Calculator.php
@@ -14,21 +14,30 @@ class Calculator
 
     public function add()
     {
-        foreach (func_get_args() as $num) {
-            if (! is_numeric($num)) {
-                throw new \InvalidArgumentException;
-            }
-            $this->result += $num;
-        }
+        $this->calculateAll(func_get_args(), '+');
     }
 
     public function subtract($int)
     {
-        foreach (func_get_args() as $num) {
-            if (! is_numeric($num)) {
+        $this->calculateAll(func_get_args(), '-');
+    }
+
+    protected function calculateAll(array $numbers, $symbol): void
+    {
+        foreach ($numbers as $num) {
+            if (!is_numeric($num)) {
                 throw new \InvalidArgumentException;
             }
-            $this->result -= $num;
+            switch ($symbol)
+            {
+                case '+':
+                    $this->result += $num;
+                    break;
+
+                case '-';
+                    $this->result -= $num;
+                    break;
+            }
         }
     }
 }
