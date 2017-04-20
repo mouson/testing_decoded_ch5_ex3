c92d831ddaa8ae96a7ceafcef9a89f4d13cdfda2
mouson <mouson@gmail.com>
2017-04-20 10:43:41 +0800

    Calculator Step 15 add Operation interface & addition implement

---
+++ app/Operation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+interface Operation
+{
+    /**
+     * Perform the arithmetic
+     *
+     * @param integer $num
+     * @param integer $current
+     *
+     * @return integer
+     */
+    public function run($num, $current);
+}
