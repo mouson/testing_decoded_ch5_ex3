7b5fb4b9dee164c544fdc82d30d70bdf8c841ec3
mouson <mouson@gmail.com>
2017-04-20 10:48:12 +0800

    Calculator Step 18 make all addition test case pass

--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -79,12 +79,14 @@ class CalculatorTest extends TestCase {
     public function testRequiresNumericValue()
     {
         /** Arrange */
+        $this->calc->setOperands("five");
+        $this->calc->setOperation(new Addition);
 
         /** Assume */
         $this->expectException(\InvalidArgumentException::class);
 
         /** Act */
-        $this->calc->add('five');
+        $actual = $this->calc->calculate();
 
         /** Assert */
 
@@ -96,13 +98,14 @@ class CalculatorTest extends TestCase {
     public function testAcceptsMultipleArgs()
     {
         /** Arrange */
+        $this->calc->setOperands(1, 2, 3, 4);
+        $this->calc->setOperation(new Addition);
 
         /** Assume */
         $expected = 10;
 
         /** Act */
-        $this->calc->add(1, 2, 3, 4);
-        $actual = $this->calc->getResult();
+        $actual = $this->calc->calculate();
 
         /** Assert */
         $this->assertSame($expected, $actual);
